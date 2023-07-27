fa046b8f26366b427402d4de656604c2bf5ae863
Joseph-Jnr <josephnwobodo1@gmail.com>
2023-07-27 14:11:57 +0100

    header section component

--- src/components/blocks/header/Header.tsx
+++ src/components/blocks/header/Header.tsx
@@ -1,30 +1,23 @@
-import { Link } from 'react-router-dom'
-import { BsInstagram, BsTwitter } from 'react-icons/bs'
+import { GeometricShape, HeaderImg } from '@/assets/images'
 
 const Header = () => {
   return (
     <>
-      <header className='bg-black h-screen'>
-        <nav className='flex justify-between tr--container py-7'>
-          <div className='nav--brand'>JosephJnr</div>
-          <div className='socials flex w-16 justify-between'>
-            <Link to='/'>
-              <span>
-                <BsInstagram />
-              </span>
-            </Link>
-            <Link to='/'>
-              <span>
-                <BsTwitter />
-              </span>
-            </Link>
+      <header>
+        <div className='md:tr--flex-row-between'>
+          <div className='intro-area'>
+            <h1 className='gradient--text'>
+              Frontend <br /> Developer.
+            </h1>
+            <p>
+              Incorporating solid technical bases with creative user interface
+              design to create unique products.
+            </p>
           </div>
-        </nav>
-        <div className='area relative'>
-          <div className='welcome--text text-center text-4xl md:text-7xl font-black tr--container'>
-            <h1 className='gradient--txt'>Hi there!</h1>
-            <h1 className='gradient--text'>My name is Joseph Jnr</h1>
-          </div>
+          {/* <div className='header-image relative'>
+            <img src={GeometricShape} alt='' />
+            <img src={HeaderImg} alt='' />
+          </div> */}
         </div>
       </header>
     </>

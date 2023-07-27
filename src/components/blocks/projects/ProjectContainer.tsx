7575787d75d730bd443897ccb2f0e8010a6d11ec
Joseph-Jnr <josephnwobodo1@gmail.com>
2023-07-27 17:22:25 +0100

    project container component

--- src/components/blocks/projects/ProjectContainer.tsx
+++ src/components/blocks/projects/ProjectContainer.tsx
@@ -3,9 +3,9 @@ import { projectData } from '@/services'
 
 const ProjectContainer = () => {
   return (
-    <section>
+    <section className='project-section bright--bg py-20' id='projects'>
       <div className='tr--container'>
-        <div className='grid md:grid-cols-3 gap-10 pb-20'>
+        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-10'>
           {projectData.map((project) => (
             <Project key={project.id} {...project} />
           ))}

3e8f1b27e2309ebccf5a8cf9c465df1f5d3676c6
Joseph-Jnr <josephnwobodo1@gmail.com>
2023-07-27 17:21:14 +0100

    added contactList data

---
+++ src/services/api/contactList.tsx
@@ -0,0 +1,31 @@
+import {
+  IconBrandGithub,
+  IconBrandTwitter,
+  IconMail,
+} from '@tabler/icons-react'
+
+const contactList = [
+  {
+    id: 1,
+    icon: <IconBrandGithub />,
+    name: 'Github',
+    handle: '@Joseph-Jnr',
+    link: 'https://github.com/Joseph-Jnr',
+  },
+  {
+    id: 2,
+    icon: <IconBrandTwitter />,
+    name: 'Twitter',
+    handle: '@Josephjnr_1',
+    link: 'https://twitter.com/josephjnr_1',
+  },
+  {
+    id: 3,
+    icon: <IconMail />,
+    name: 'Email',
+    handle: 'josephnwobodo1@gmail.com',
+    link: '/',
+  },
+]
+
+export default contactList

6e01f113b7ba2b38882946c21d17c8464f26a90d
Joseph-Jnr <josephnwobodo1@gmail.com>
2023-07-27 17:20:24 +0100

    contact component

---
+++ src/components/blocks/contact/SingleContact.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from 'react'
+import { Avatar } from '@mantine/core'
+import { Link } from 'react-router-dom'
+
+type SingleContactProps = {
+  icon: ReactNode
+  name: string
+  handle: string
+  link: string
+}
+
+const SingleContact = ({ icon, name, handle, link }: SingleContactProps) => {
+  return (
+    <div className='single-contact'>
+      <div className='flex gap-5'>
+        <div className='icon'>
+          <Avatar color='teal' radius='xl'>
+            {icon}
+          </Avatar>
+        </div>
+        <div className='text'>
+          <h1>{name}</h1>
+          <Link to={link}>
+            <p>{handle}</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SingleContact

1c937f94beae5bfb51a3cae90231335b5a65089b
Joseph-Jnr <josephnwobodo1@gmail.com>
2023-07-27 14:08:15 +0100

    new jpegs

---
+++ src/assets/images/jpg/index.ts
@@ -0,0 +1,6 @@
+export { default as HeaderImg } from './IMG-20230608-WA0015.jpg'
+export { default as Trouve } from './trouve.png'
+export { default as Foodify } from './foodify.png'
+export { default as Bic } from './bic.png'
+export { default as BicAdmin } from './bic-admin.png'
+export { default as AntiSapa } from './anti-sapa.png'

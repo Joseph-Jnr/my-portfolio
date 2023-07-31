12034843f9c0fcaa3749fc9a9e8b6c5237a315d0
Joseph-Jnr <josephnwobodo1@gmail.com>
2023-07-31 20:00:27 +0100

    Initiated aos animation

--- src/pages/website/Home.tsx
+++ src/pages/website/Home.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react'
 import { About, Contact, Header, Navbar, ProjectContainer } from '@/components'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 const Home = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+    })
+  }, [])
   return (
     <>
       <main>
